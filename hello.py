57fbf26b63b713cd922d0e6e2c02fa2786456f13
luYunYxz <lu_yun_yxz@163.com>
2023-06-27 15:35:39 +0800

    字符串 f 替换变量

--- hello.py
+++ hello.py
@@ -5,4 +5,4 @@ this is the test many lines comments
 '''
 name = input("what is your name?")
 # print(*objects,seq=' ',end='\n'); 当传入多个参数的俄时候,以空格分割符,以\n作为结尾
-print("hello, \"my friend\"" , name , end="")
+print(f"hello, \"my friend\" {name}" ,  end="")
