8244b826a3c0907f825e9d31cb570941e2cf36dd
luYunYxz <lu_yun_yxz@163.com>
2023-06-27 21:23:37 +0800

    使用普通的hello方式

--- hello.py
+++ hello.py
@@ -1,15 +1,2 @@
-# Ask user for their name
-
-'''
-this is the test many lines comments
-'''
-name = input("what is your name?")
-name = name.strip()
-name = name.title()
-first, second = name.split()
-# print(*objects,seq=' ',end='\n'); 当传入多个参数的俄时候,以空格分割符,以\n作为结尾
-print(f"hello, \"my friend\" {name}" ,  end="")
-
-print(f"the first name = {first}")
-
-print(f"the second = {second}")
+name = input("what is your name? ")
+print("hello ? " , name)
