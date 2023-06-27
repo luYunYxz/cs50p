6148b2c7e2d2511f39277cff90cfff10d56fa122
luYunYxz <lu_yun_yxz@163.com>
2023-06-27 20:37:13 +0800

    添加了str的strip和title

--- hello.py
+++ hello.py
@@ -6,6 +6,8 @@ this is the test many lines comments
 name = input("what is your name?")
 name = name.strip()
 name = name.title()
-
+first, second = name.split;
 # print(*objects,seq=' ',end='\n'); 当传入多个参数的俄时候,以空格分割符,以\n作为结尾
 print(f"hello, \"my friend\" {name}" ,  end="")
+
+print(f"the first name = {first}")
