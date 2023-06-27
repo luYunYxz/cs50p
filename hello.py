954ae0f76e764cd529f7d98b94f81a6a04000467
luYunYxz <lu_yun_yxz@163.com>
2023-06-27 20:38:16 +0800

    添加了split

--- hello.py
+++ hello.py
@@ -6,7 +6,7 @@ this is the test many lines comments
 name = input("what is your name?")
 name = name.strip()
 name = name.title()
-first, second = name.split;
+first, second = name.split()
 # print(*objects,seq=' ',end='\n'); 当传入多个参数的俄时候,以空格分割符,以\n作为结尾
 print(f"hello, \"my friend\" {name}" ,  end="")
 
