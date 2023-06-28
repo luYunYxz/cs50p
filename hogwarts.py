955de1ddec350b4926f3fbfa61b9a81737c25946
luYunYxz <lu_yun_yxz@163.com>
2023-06-28 12:02:58 +0800

    适用 [{},{}]得方式来表示多个变量和属性

--- hogwarts.py
+++ hogwarts.py
@@ -1,9 +1,11 @@
-students = {
-    "Hermione":"Gryffindor",
-    "Harry":"Gryffyindor",
-    "Ron":"Gryffyiondor",
-    "Draco":"Slytherin"
-}
+students = [
+    {"name":"Hermione","house":"Gryffindor","patronus":"Otter"},
+    {"name":"Herry","house":"Gryffindor","patronus":"Stag"},
+    {"name":"Ron","house":"Gryffindor","patronus":"Jack Russell terrier"},
+    {"name":"Draco","house":"Slytherin","patronus":None}
+]
+
 
 for student in students:
-    print(student ,students[student],sep="->")
+    print(student["name"],student["house"],student["patronus"],sep=" , ")
+    
