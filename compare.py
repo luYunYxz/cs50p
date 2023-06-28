00299e41219d7f719bc1323ec78e673abe6aa44d
luYunYxz <lu_yun_yxz@163.com>
2023-06-28 09:35:46 +0800

    添加了 逻辑表达式 or

--- compare.py
+++ compare.py
@@ -1,10 +1,7 @@
 x = int(input("what is x?"))
 y = int(input("what is y?"))
 
-
-if x < y:
-    print("x is less than y") 
-elif x > y:
-    print("x is great than y")
+if x < y or x > y:
+    print("x is not equal y")
 else:
     print("x is equal y") 
