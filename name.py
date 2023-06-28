14b27d9cf66558ef05de38bcc6e0e5a1db563de4
luYunYxz <lu_yun_yxz@163.com>
2023-06-28 18:37:22 +0800

    使用 for loop 访问所有的sys.argv参数

--- name.py
+++ name.py
@@ -2,7 +2,6 @@ import sys
 
 if len(sys.argv) < 2:
     sys.exit("the args to few")
-elif len(sys.argv) >2:
-    sys.exit("the args to log")
-else:
-    print("Hello , ",sys.argv[1])
+
+for areg in sys.argv:
+    print("hello ,",areg)
