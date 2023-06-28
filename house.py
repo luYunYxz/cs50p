9cb001dd6f8429a398760c1e643d07122fa07f45
luYunYxz <lu_yun_yxz@163.com>
2023-06-28 10:28:30 +0800

    使用if else测试多种分支的代码,为match(switch)做准备

---
+++ house.py
@@ -0,0 +1,10 @@
+name = input("what is your name? ")
+
+if name == "Harry":
+    print("Gryffindor")
+elif name == "Hermione":
+    print("Gryffinodr")
+elif name == "Draco":
+    print("Slytherin")
+else:
+    print("Who?")
