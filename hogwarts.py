f123f5676d4f1159f951a5ba39cf8b9e7ca52d1f
luYunYxz <lu_yun_yxz@163.com>
2023-06-28 11:40:48 +0800

    添加了dict类型,其中的定义使用大括号:,分割的键值对.访问的时候适用dict[key]的方式来获取到value

--- hogwarts.py
+++ hogwarts.py
@@ -1,4 +1,11 @@
-students = ["Hermione","Harry","Ron"]
+students = {
+    "Hermione":"Gryffindor",
+    "Harry":"Gryffyindor",
+    "Ron":"Gryffyiondor",
+    "Draco":"Slytherin"
+}
 
-for i in range(len(students)):
-    print(i+1,students[i])
+print(students["Hermione"])
+print(students["Ron"])
+print(students["Harry"])
+print(students["Draco"])
