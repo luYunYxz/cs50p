162b3abd35f28c216b83e3b5c79026dda3eb846b
luYunYxz <lu_yun_yxz@163.com>
2023-06-28 12:21:44 +0800

    使用print(*n)的方式帮助实现打印

--- mario.py
+++ mario.py
@@ -3,8 +3,5 @@ def main():
     print_square(5)
 def print_square(n):
     for _ in range(n):
-        for _ in range(n):
-            print("#",end="")
-        print()
-        
+        print("#"*n)
 main()
