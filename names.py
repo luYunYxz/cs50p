d2ea96f82b215c979287cad307a4d002d6590e7e
luYunYxz <lu_yun_yxz@163.com>
2023-06-29 11:32:41 +0800

    使用了file.oepn获取到了文件句柄,然后又使用了 line.rstrip()将多余的换行符去除

--- names.py
+++ names.py
@@ -1,4 +1,5 @@
-name = input("what is your name")
+with open("names.txt","r") as file:
+    lines = file.readlines();
 
-with open("names.txt","a") as file:
-    file.write(f"{name}\n")
+for line in lines:
+    print("hello, ", line.rstrip())
