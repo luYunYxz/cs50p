609cbb691c463f17ba3a2a4b62ef76c1af49c3d9
luYunYxz <lu_yun_yxz@163.com>
2023-06-29 19:49:28 +0800

    当csv中包括了标题,这个时候适用DictReader阅读方式,获取到reader.然后在每一行中都可以适用[title]的方式访问

--- student.py
+++ student.py
@@ -2,9 +2,9 @@ import csv
 students = []
 
 with open("student.csv") as file:
-    reader = csv.reader(file)
+    reader = csv.DictReader(file)
     for line in reader:
-        students.append({"name":line[0],"house":line[1]})
+        students.append({"name":line["name"],"house":line["house"]})
 
 for student in sorted(students,key=lambda student : student["name"],reverse=True):
     print(f"{student['name']} is in {student['house']}")
