893a9cb30125bdbd6d8487b2af5043d97a10b6a5
luYunYxz <lu_yun_yxz@163.com>
2023-07-04 16:02:47 +0800

    使用 := 表示计算出一个函数值,并将其赋值给左边的元素

--- format.py
+++ format.py
@@ -1,9 +1,8 @@
 import re
 name = input("what is your name ? ").strip()
 
-matcher = re.search("(.+), *(.+)",name)
 
-if matcher:
+if matcher := re.search("(.+), *(.+)",name):
     first = matcher.group(1)
     second = matcher.group(2)
     name = f"{first} {second}"
