661aad7505f6a8f60efce82926cafe4de82a32a2
luYunYxz <lu_yun_yxz@163.com>
2023-07-07 09:41:41 +0800

    在类中定义一个 __add__(self,other)方法来实现重载类的的+运算法

--- vault.py
+++ vault.py
@@ -7,7 +7,12 @@ class Valut:
 
     def __str__(self):
         return f"{self.galleons} galleons, {self.sickles} sickles, {self.knuts} knuts"
-
+    
+    def __add__(self,other):
+        galleons = self.galleons + other.galleons
+        sickles = self.sickles + other.sickles
+        knuts = self.knuts + other.knuts
+        return Valut(galleons,sickles,knuts)
 
 potter = Valut(100,50,25)
 print(potter)
@@ -15,12 +20,6 @@ print(potter)
 weasley = Valut(25,50,100)
 print(weasley)
 
-galleons = potter.galleons + weasley.galleons
-sickles = potter.sickles + weasley.sickles
-knuts = potter.knuts + weasley.knuts
-
-total = Valut(galleons,sickles,knuts)
-
-print(total)
+print(potter + weasley)
 
 
