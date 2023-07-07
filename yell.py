2e4d97ab0533b8bb4412907780d0f8de1ead3549
luYunYxz <lu_yun_yxz@163.com>
2023-07-07 18:15:23 +0800

    使用了 word.upper for in words if word....这种list写法帮助来简化访问list并操作list的实现

--- yell.py
+++ yell.py
@@ -2,7 +2,7 @@ def main():
     yell("This" ,"is","CS50")
 
 def yell(*words):
-    temp = map(str.upper,words) 
+    temp = [word.upper()for word in words]
     print(*temp)
 
 if __name__ == "__main__":
